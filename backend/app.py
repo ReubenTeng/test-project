377106b8d6a547e3d24edf4f4e1c9e604aca38c7
Reuben Teng <reubentengjw@gmail.com>
2023-07-14 13:53:46 +0800

    Added persistence with psql and historic data

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
-from coinbase_accessor import get_coinbase_data
+import coinbase_accessor as ca
+import signal
+import threading
+import uvicorn
 
 app = FastAPI()
 
@@ -12,6 +15,33 @@ async def root():
 @app.get("/exchange-rates")
 async def exchange_rates(base: str):
     try:
-        return get_coinbase_data(base)
+        connection = ca.connect_to_database()
+        result = ca.get_coinbase_data(base, connection)
+        connection.close()
+        return result
     except ValueError:
         return {"message": "Invalid base value. Must be either 'fiat' or 'crypto'"}
+
+
+@app.get("/historical-rates")
+async def get_historical_rates(
+    base_currency: str, target_currency: str, start: int, end: int = -1
+):
+    try:
+        connection = ca.connect_to_database()
+        result = ca.get_historical_rates(
+            base_currency, target_currency, start, end, connection=connection
+        )
+        connection.close()
+        return result
+    except ValueError:
+        return {"message": "Unable to find data for the given currency pair"}
+
+
+def update_database():
+    ca.update_all()
+    threading.Timer(30.0, ca.update_all).start()
+
+
+# ca.create_table()
+update_database()
--- backend/coinbase_accessor.py
+++ backend/coinbase_accessor.py
@@ -1,7 +1,12 @@
 import requests as re
+import psycopg2 as pg
+import time
+
+FIAT = ["USD", "SGD", "EUR"]
+CRYPTO = ["BTC", "ETH", "DOGE"]
 
 
-def get_coinbase_data(curr_type: str):
+def get_coinbase_data(curr_type: str, connection=None):
     """Sends GET request to coinbase free API and returns the data of either FIAT or Crypto depending on type.
 
     Args:
@@ -9,50 +14,150 @@ def get_coinbase_data(curr_type: str):
     """
     if curr_type != "fiat" and curr_type != "crypto":
         raise ValueError("curr_type must be either 'fiat' or 'crypto'")
-    url = "https://api.coinbase.com/v2/exchange-rates?currency="
+
     rates = {}
     if curr_type == "fiat":
-        response = re.get(url + "USD")
-        result = response.json()["data"]["rates"]
-        rates["USD"] = {
-            "BTC": result["BTC"],
-            "ETH": result["ETH"],
-            "DOGE": result["DOGE"],
-        }
-        response = re.get(url + "SGD")
-        result = response.json()["data"]["rates"]
-        rates["SGD"] = {
-            "BTC": result["BTC"],
-            "ETH": result["ETH"],
-            "DOGE": result["DOGE"],
-        }
-        response = re.get(url + "EUR")
-        result = response.json()["data"]["rates"]
-        rates["EUR"] = {
-            "BTC": result["BTC"],
-            "ETH": result["ETH"],
-            "DOGE": result["DOGE"],
-        }
+        for curr in FIAT:
+            rates[curr] = get_curr(curr, "fiat", connection)
     elif curr_type == "crypto":
-        response = re.get(url + "BTC")
-        result = response.json()["data"]["rates"]
-        rates["BTC"] = {
-            "USD": result["USD"],
-            "SGD": result["SGD"],
-            "EUR": result["EUR"],
-        }
-        response = re.get(url + "ETH")
-        result = response.json()["data"]["rates"]
-        rates["ETH"] = {
-            "USD": result["USD"],
-            "SGD": result["SGD"],
-            "EUR": result["EUR"],
-        }
-        response = re.get(url + "DOGE")
-        result = response.json()["data"]["rates"]
-        rates["DOGE"] = {
-            "USD": result["USD"],
-            "SGD": result["SGD"],
-            "EUR": result["EUR"],
-        }
+        for curr in CRYPTO:
+            rates[curr] = get_curr(curr, "crypto", connection)
     return rates
+
+
+def get_curr(curr: str, base, connection=None):
+    """Gets the most recent conversion rates for a given currency.
+
+    Args:
+        curr (str): currency to convert from
+        base (_type_): base (either fiat or crypto) of the given currency
+        connection (_type_, optional): If given, uses psql server. Else, takes from coinbase API. Defaults to None.
+
+    Raises:
+        ValueError: _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if base != "fiat" and base != "crypto":
+        raise ValueError("curr_type must be either 'fiat' or 'crypto'")
+    if connection is None:
+        url = "https://api.coinbase.com/v2/exchange-rates?currency="
+        response = re.get(url + curr)
+        result = response.json()["data"]["rates"]
+        if base == "fiat":
+            return {"BTC": result["BTC"], "ETH": result["ETH"], "DOGE": result["DOGE"]}
+        elif base == "crypto":
+            return {"USD": result["USD"], "SGD": result["SGD"], "EUR": result["EUR"]}
+    else:
+        rates = {}
+        cursor = connection.cursor()
+        if base == "fiat":
+            for curr_to in CRYPTO:
+                # get the most recent rate
+                cursor.execute(
+                    f"SELECT rate FROM conversion WHERE from_curr = '{curr}' AND to_curr = '{curr_to}' ORDER BY timestamp DESC LIMIT 1;"
+                )
+                rates[curr_to] = cursor.fetchone()[0]
+        elif base == "crypto":
+            for curr_to in FIAT:
+                cursor.execute(
+                    f"SELECT rate FROM conversion WHERE from_curr = '{curr}' AND to_curr = '{curr_to}' ORDER BY timestamp DESC LIMIT 1;"
+                )
+                rates[curr_to] = cursor.fetchone()[0]
+        cursor.close()
+        return rates
+
+
+def connect_to_database():
+    # DB schema:
+    # the primary key is (from_curr, to_curr, timestamp)
+    # from_curr: the currency to convert from
+    # to_curr: the currency to convert to
+    # rate: the conversion rate from from_curr to to_curr
+    # timestamp: the time the rate was last updated
+
+    NAME: str = "curr_exchange"
+    USER: str = "USERNAME"
+    PASSWORD: str = "PASSWORD"
+    HOST: str = "HOST"
+    PORT: str = "PORT"
+    connection = pg.connect(
+        database=NAME, user=USER, password=PASSWORD, host=HOST, port=PORT
+    )
+
+    return connection
+
+
+def get_historical_rates(
+    base_curr: str,
+    to_curr: str,
+    start_time: int,
+    end_time: int = -1,
+    connection=None,
+):
+    """Gets the historical rates for a given currency pair.
+
+    Args:
+        base_curr (str): Base currency
+        to_curr (str): Currency to convert to
+        start_time (int): Start time of the query
+        end_time (_type_, optional): End time of the query. Defaults to current time.
+        connection (_type_): Connection to the database
+    """
+    to_close = False
+    if connection is None:
+        to_close = True
+        connection = connect_to_database()
+    cursor = connection.cursor()
+    command = (
+        f"SELECT rate, timestamp FROM conversion WHERE from_curr = '{base_curr}' AND to_curr = '{to_curr}' AND timestamp >= {start_time} AND timestamp <= {end_time} ORDER BY timestamp ASC;"
+        if end_time != -1
+        else f"SELECT rate, timestamp FROM conversion WHERE from_curr = '{base_curr}' AND to_curr = '{to_curr}' AND timestamp >= {start_time} ORDER BY timestamp ASC;"
+    )
+    cursor.execute(command)
+    response = cursor.fetchall()
+    result = {}
+    result["result"] = []
+    for row in response:
+        result["result"].append({"value": row[0], "timestamp": row[1]})
+    cursor.close()
+    if to_close:
+        connection.close()
+    return result
+
+
+def update_all(connection=None):
+    """Updates all values in the database.
+
+    Args:
+        connection: connection to the database
+    """
+    to_close = False
+    if connection is None:
+        to_close = True
+        connection = connect_to_database()
+
+    cursor = connection.cursor()
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS conversion (from_curr TEXT, to_curr TEXT, rate TEXT, timestamp INTEGER, PRIMARY KEY (from_curr, to_curr, timestamp));"
+    )
+    rates = get_coinbase_data("fiat")
+    timestamp = int(time.time())
+    for curr in rates:
+        for curr_to in rates[curr]:
+            cursor.execute(
+                f"INSERT INTO conversion (from_curr, to_curr, rate, timestamp) VALUES ('{curr}', '{curr_to}', {rates[curr][curr_to]}, {timestamp}) ON CONFLICT (from_curr, to_curr, timestamp) DO UPDATE SET rate = {rates[curr][curr_to]};"
+            )
+            print("updated " + curr + " -> " + curr_to + " to " + rates[curr][curr_to])
+    rates = get_coinbase_data("crypto")
+    for curr in rates:
+        for curr_to in rates[curr]:
+            cursor.execute(
+                f"INSERT INTO conversion (from_curr, to_curr, rate, timestamp) VALUES ('{curr}', '{curr_to}', {rates[curr][curr_to]}, {timestamp}) ON CONFLICT (from_curr, to_curr, timestamp) DO UPDATE SET rate = {rates[curr][curr_to]};"
+            )
+            print("updated " + curr + " -> " + curr_to + " to " + rates[curr][curr_to])
+    connection.commit()
+    cursor.close()
+    if to_close:
+        connection.close()
